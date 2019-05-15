8f441e097fc3583d1be47b6be3e875670b1c86f1
Kathleen Howard <kmaryhoward@gmail.com>
2019-05-15 13:58:51 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project_name)
+    project = Project.new(project_name)
+    self.backed_projects << project
+  end
+
+  
+
+
+end
